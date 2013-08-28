19465ce7b3c4bc41edc98eb3924a2ab16903d902
kurochan <kuro@kurochan.org>
2013-08-28 12:12:06 +0900

    ユーザ登録処理

--- app/Controller/LoginController.php
+++ app/Controller/LoginController.php
@@ -13,12 +13,28 @@ class LoginController extends AppController{
             $this->Session->write('login', 'true');
             $this->Session->delete('controller');
             $this->Session->delete('action');
+            $user_check();
             $this->redirect($redirect);
         } else {
-            $this->Session->write('controller', $this->params['url']['co']);
-            $this->Session->write('action', $this->params['url']['ac']);
+            if($this->params['url']['co'] && $this->params['url']['ac']) {
+                $this->Session->write('controller', $this->params['url']['co']);
+                $this->Session->write('action', $this->params['url']['ac']);
+            }
             $facebook = self::getFacebook();
             $this->redirect(self::getFacebook()->getLoginUrl());
         }
+    }
+
+    private function user_check() {
+        $facebook = self::getFacebook();
+        $profile = $facebook->api('/me');
+        var_dump($profile);
+        exit();
+        $id = 1;
+        $name = "テスト";
+        $redis = self::getRedis();
+        $status = $redis->sadd('user_id_set', $id);
+        $redis->hset('user_'.$id, 'name', $name);
+        return $status ? true : false;
     }
 }
